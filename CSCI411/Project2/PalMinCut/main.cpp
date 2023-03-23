106df60e12486bce08b5e26d802c36b8c2f4f5d5
Caleb Besser <cebesser@csuchico.edu>
2023-03-22 18:20:12 -0700

    CSCI411

--- CSCI411/Project2/PalMinCut/main.cpp
+++ CSCI411/Project2/PalMinCut/main.cpp
@@ -10,7 +10,55 @@ int main(){
         string myString;
         cin >> myString;
         cout << myString << "\n";
-
+        int len = myString.size();
+        vector<vector<bool>> P(len, vector<bool>(len, 0));
+        vector<int> C(len, 0);
+        vector<int> B(len, -1);
+        is_pal(P, myString);
+        C[0] = 0;
+        B[0] = -1;
+        for(int i = 1; i < len; i++){
+            if(P[0][i] == 1){
+                C[i] = 0;
+                B[i] = -1;
+            }else{
+                for(int j = 0; j < i; j++){
+                    if(P[j+1][i] == 1){
+                        C[i] = C[j] + 1;
+                        B[i] = j;
+                        break;
+                    }
+                }
+            }
+           
+        }
+        /*
+        loop through each element i
+        for each i loop from j to i, j from 0
+        update C and B for each element based on rules on pdf
+        */
+        cout << "C: ";
+        for(int i = 0; i < len; i++){
+            cout << C[i] << " ";
+        }
+        cout << "\n";
+        cout << "B: ";
+        for(int i = 0; i < len; i++){
+            cout << B[i] << " ";
+        }
+        cout << "\n";
+        cout << "   ";
+        for(int j = 0; j < len; j++){
+            cout << myString[j] << " ";
+        }
+        cout << "\n";
+        for(int i = 0; i < len; i++){
+            cout << myString[i] << ": ";
+            for(int j = 0; j < len; j++){
+                cout << P[i][j] << " ";
+            }
+            cout << "\n";
+        }
         return 0;
 }//main()
 
